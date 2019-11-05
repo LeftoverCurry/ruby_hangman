f5c088bb3c985106c9ddd73ac4773cd8ab109c0c
Tripp Martin <tripp.martin3@gmail.com>
2019-11-05 13:20:05 -0500

    Created letters display

--- hangmantesting.rb
+++ hangmantesting.rb
@@ -13,27 +13,31 @@ class Game
     end
     @chosen_letters = chosen_letters
     @letters_remaining = letters_remaining
-    user_makes_guess(@chosen_letters, @letters_remaining)
+    
+    user_makes_guess
   end
 
   def create_magic_word
     word_file = '5desk.txt'
     random_word = File.readlines(word_file).sample.strip
+    @MAGIC_WORD_CONST = random_word.split('')
     random_word.split('')
   end
 
-  def user_makes_guess(chosen_letters = @chosen_letters , letters_remaining = @letters_remaining)
+  def user_makes_guess
     check_for_win_or_loss
     show_image
+    letters_display
     puts 'Please enter a letter'
-    letter = gets.chomp.downcase
-      if @magic_word.include? letter
-        guess_was_correct(letter)
-      else 
-        puts 'That letter is not included!'
-        @letters_remaining -= 1
-        user_makes_guess(@chosen_letters, @letters_remaining)
-      end
+    @user_letter = gets.chomp.downcase
+    @chosen_letters << @user_letter
+    if @magic_word.include? @user_letter
+      guess_was_correct(@user_letter)
+    else
+      puts 'That letter is not included!'
+      @letters_remaining -= 1
+      user_makes_guess
+    end
   end
 
   def guess_was_correct(letter)
@@ -71,10 +75,10 @@ class Game
   
   def check_for_win_or_loss
     if @magic_word == []
-        `say "You Won! CONGRATULATIONS!"`
-        display = File.read('./art/winscreen')
-        puts display
-        exit
+      `say "You Won! CONGRATULATIONS!"`
+      display = File.read('./art/winscreen')
+      puts display
+      exit
     elsif @letters_remaining <= 0
       display = File.read('./art/you_dead')
       puts display
@@ -85,7 +89,16 @@ class Game
   end
 
   def letters_display
-
+    missing_letters = @MAGIC_WORD_CONST - @chosen_letters
+    display_array = []
+    @MAGIC_WORD_CONST.each do |letter| 
+      if missing_letters.include? letter
+        display_array << ' _ '
+      else
+        display_array << letter
+      end
+    end
+    puts display_array.join
   end
 end
 
